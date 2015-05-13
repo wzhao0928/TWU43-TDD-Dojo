1e344b7dfa08f8d0240a496b01ed3a59d5bb738b
wzhao0928 <wbzhao@thoughtworks.com>
2015-05-13 22:14:26 +0530

    [Wenbo] re-submit the previous wrong commit message

--- test/tdddojo/MommyTests.java
+++ test/tdddojo/MommyTests.java
@@ -19,5 +19,6 @@ public class MommyTests {
     public void testMommyShouldReplaceSingleVowelCharWhenVowelsMoreThan30Percent() throws Exception {
         assertEquals("bmommyb", Mommy.mommify("bab"));
 
+
     }
 }
